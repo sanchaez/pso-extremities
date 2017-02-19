194079cfd3e449ddc7db5f59d3ad8302f13de1a6
Oleksandr Shaposhnikov <o.shaposhnikov-2020@kpi.ua>
2017-02-19 22:22:48 +0200

    Template for classic PSO

--- pso-extremities/pso-extremities-test/pso.h
+++ pso-extremities/pso-extremities-test/pso.h
@@ -1,15 +1,57 @@
-/* PSO DLL interface */
-
-//  This header is intended to be used 
-//  for importing and exporting DLL symbols.
-
+/* PSO interface */
 
 #pragma once
 #include <cmath>
-#include <valarray>
+#include <functional>
 #include <random>
+#include <valarray>
 
 namespace pso {
 
-  //TODO: Add exported types and classes
+using size_t = std::size_t;
+template <typename value_t>
+using container_t = std::valarray<value_t>;
+template <typename value_t>
+using dimension_limit_t = std::pair<value_t, value_t>;
+template <typename value_t>
+using comparing_predicate_t = std::function<bool(value_t, value_t)>;
+template <typename value_t>
+using function_t = std::function<value_t(container_t<value_t>)>;
+template <typename value_t>
+using result_t = std::pair<value_t, container_t<value_t>>;
+template <typename value_t>
+using dimention_container_t = container_t<dimension_limit_t<value_t>>;
+
+template <typename value_t>
+struct Particle {
+  Particle(const container_t<value_t> &coordinates, const value_t &velocity)
+      : x(coordinates), pbest(coordinates), v(velocity) {}
+  value_t v;
+  container_t<value_t> x;
+  container_t<value_t> pbest;
+};
+template <typename value_t>
+using particle_container_t = container_t<Particle<value_t>>;
+
+template <typename value_t>
+class PSOClassic {
+ public:
+  PSOClassic(const function_t& function,
+             const dimention_container_t<value_t>& boudaries,
+             const value_t& max_velocity)
+      : vmax(max_velocity), bounds(boundaries), f(function) {}
+
+  result_t<value_t> operator(const comparing_predicate_t& condition) {
+    initialize_particles();
+
+  }
+
+ private:
+  void initialize_particles(){};
+  const value_t vmax;
+  const dimention_container_t<value_t> bounds;
+  const function_t f;
+  container_t<value_t> gbest;
+  particle_container_t<value_t> particles(50);
+};
 }
